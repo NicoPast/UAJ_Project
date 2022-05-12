65fe5321bb1b68f7e174f776a45a27f6888cefd1
davidczepiel <dczepiel@ucm.es>
2022-05-12 16:22:55 +0200

    Avances procesamiento de datos
    Programadores o no programadores y clases auxiliares
    
    Co-Authored-By: Antgue01 <48839258+Antgue01@users.noreply.github.com>

--- Parsing/processData.py
+++ Parsing/processData.py
@@ -1,12 +1,19 @@
 from email.utils import formatdate
-import operator
+from sqlalchemy import false
 from Parsing import hacerDiccionario,prettify
 from datetime import datetime
 import xml.etree.ElementTree as ET
+import csv
+
+
+#Clase que representa la info de un Nivel que ha realizado un Probador
 class levelInfo:
     tries=0
     playTime=0
     codeLenght=0
+
+
+#Clase que recopila informacion sobre tiempo jugado en un nivel 
 class timeInfo:
     levelName = ""
     levelTime = 0   
@@ -15,80 +22,110 @@ class timeInfo:
         self.levelTime=time
 
 
+#Clase que recopila la información de un probador 
+class Tester:
+    #Si ha recibido clases de programacion o no
+    programador = False
+    #Diccionario que tiene una entrada por nivel jugado con informacion de su desempeño en cada uno
+    infoLevels = dict()
+
+
 def main():
 
+    #Obtenemos las trazas limpias sobre los probadores de una sesion
     trazas = prettify(hacerDiccionario())
-    #Sacar el número de niveles que hay
-    #Diccionario una entrada por persona => pepito que tiene una entrada por nivel => {info: {tiempo tardado, num intentos, cosas pistas, cosas código, " sabe de programación o no"}}
-    numPersonas = 100
+    #Dicccionario que va a tener una entrada por probador
     Probadores = {}
 
-    #Por cada persona recorro todos los niveles
+    #Recorremos todas las personas de las trazas
     for nombreProbador in trazas.keys():
-
-        #Preparar diccionario
-        Probadores[nombreProbador]= dict()
+        Probadores[nombreProbador]= Tester()
         duracionesNiveles = [] 
 
     #################################Número de intentos por nivel: sumar n de success + n de failed, por nivel #####################################
+
+        #Niveles que ha completado correctamente
         for succesfull in trazas[nombreProbador]["SuccessLevels"]:
-            
             nombreNivel = succesfull["object"]["id"]
             timestamp = succesfull["timestamp"]
-            if(not nombreNivel in Probadores[nombreProbador]):            
-                Probadores[nombreProbador][nombreNivel]=levelInfo()
+            #En caso de que no tenga info sobre este nivel se la añadimos
+            if(not nombreNivel in Probadores[nombreProbador].infoLevels):            
+                Probadores[nombreProbador].infoLevels[nombreNivel]=levelInfo()
+
+            #Pasamos la fecha del evento a un timestamp util para nosotros
             format_date = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
             converted_timestamp=format_date.timestamp()
+
             #Aumentamos contador con el número de veces que se ha completado cada nivel
-            Probadores[nombreProbador][nombreNivel].tries =  Probadores[nombreProbador][nombreNivel].tries+1
+            Probadores[nombreProbador].infoLevels[nombreNivel].tries =  Probadores[nombreProbador].infoLevels[nombreNivel].tries+1
             duracionesNiveles.append(timeInfo(nombreNivel, converted_timestamp))
 
 
+        #Recorremos los niveles en los que haya fallado
         for failed in trazas[nombreProbador]["FailedLevels"]:
-            #Comprobación de que existe y tal
             nombreNivel = failed["object"]["id"]
             timestamp = failed["timestamp"]
-            if(not nombreNivel in Probadores[nombreProbador]):            
-                Probadores[nombreProbador][nombreNivel]=levelInfo()
+            #En caso de que no tenga info sobre este nivel se la añadimos
+            if(not nombreNivel in Probadores[nombreProbador].infoLevels):            
+                Probadores[nombreProbador].infoLevels[nombreNivel]=levelInfo()
 
-            #Aumentamos contador con los intentos fallidos de dicho nivel
+            #Pasamos la fecha del evento a un timestamp util para nosotros
             format_date = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
             converted_timestamp=format_date.timestamp()
-            Probadores[nombreProbador][nombreNivel].tries =  Probadores[nombreProbador][nombreNivel].tries+1
+
+            #Aumentamos contador con el número de veces que se ha completado cada nivel
+            Probadores[nombreProbador].infoLevels[nombreNivel].tries =  Probadores[nombreProbador].infoLevels[nombreNivel].tries+1
             duracionesNiveles.append(timeInfo(nombreNivel, converted_timestamp))
 
 
     ################################# Longitud del código  #####################################
+        #Por cada nivel jugado miramos el codigo que mandó y contamos los bloques que lo forman
         for intento in trazas[nombreProbador]["LevelTries"]:
-            #Comprobación de que existe y tal
             nombreNivel = intento["object"]["id"]
-            if(not nombreNivel in Probadores[nombreProbador]):            
-                Probadores[nombreProbador][nombreNivel]=levelInfo()
+
+            #En caso de que no tenga info sobre este nivel se la añadimos
+            if(not nombreNivel in Probadores[nombreProbador].infoLevels):            
+                Probadores[nombreProbador].infoLevels[nombreNivel]=levelInfo()
 
             #Obtenemos el codigo de la ultima vez que se ejecutó el nivel y calculamos su longitud
             codigo = intento["result"]["extensions"]["code"]
             xml= ET.ElementTree(ET.fromstring(codigo))
-            cont=0
+            #Por cada aparicion del bloque 'block' aumentamos el contador de instrucciones
+            instructions=0
             for block in xml.iter('block'):
-                cont+=1
-            Probadores[nombreProbador][nombreNivel].codeLenght =cont #calculo de la hostia
+                instructions+=1
+            Probadores[nombreProbador].infoLevels[nombreNivel].codeLenght = instructions 
 
 
     ################################# Duracion de cada nivel #####################################
     #Ordenar el array
-        duracionesNiveles.sort(key=operator.attrgetter('levelTime'))
-        pos = len(duracionesNiveles)-1
-        nivelAAnalizar=""
-        while pos != 0 and nivelAAnalizar != duracionesNiveles[0].levelName:
-            duracionNivelActual=0
-            nivelAAnalizar = duracionesNiveles[pos].levelName
-            while duracionesNiveles[pos-1].levelName == nivelAAnalizar:
-                duracionNivelActual+= Probadores[nombreProbador][nivelAAnalizar].playTime #calculo de parsear de string a numeros 
-                pos = pos-1
-            
+        # duracionesNiveles.sort(key=operator.attrgetter('levelTime'))
+        # pos = len(duracionesNiveles)-1
+        # nivelAAnalizar=""
+        # while pos != 0 and nivelAAnalizar != duracionesNiveles[0].levelName:
+        #     duracionNivelActual=0
+        #     nivelAAnalizar = duracionesNiveles[pos].levelName
+        #     while duracionesNiveles[pos-1].levelName == nivelAAnalizar:
+        #         duracionNivelActual+= Probadores[nombreProbador][nivelAAnalizar].playTime #calculo de parsear de string a numeros 
+        #         pos = pos-1
 
-    a=2
+    #############Obtención de programadores o no programadores (estan en otro fichero)
+    #Leemos el csv entero
+    datosCSV=[]
+    with open('results-survey945759.csv', 'r') as file:
+        #Como delimitador ponemos los ;
+        reader = csv.reader(file,delimiter=';')
+        for each_row in reader:
+            datosCSV.append(each_row)
 
+    columnaProgramacion = 5
+    #Sacar quien es programador o no de todos los probadores
+    for pos in range(1,len(datosCSV)):
+        if datosCSV[pos][columnaProgramacion] == "No":
+            Probadores[datosCSV[pos][1]].programador = False
+        else:
+            Probadores[datosCSV[pos][1]].programador = True
+    
 
 main()
 
